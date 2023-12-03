bd1120fd02c56d9e0a85129637cbbfd387952fae
TheDarkTiger <thedarktiger@hotmail.com>
2023-12-03 23:43:48 +0100

    Add rand. Add daily menu.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 
 /* ======================================================================== */
@@ -10,7 +12,7 @@ typedef struct t_game
 {
 	bool initilized;
 	unsigned long days;
-	unsigned char health;
+	signed char health;
 	unsigned long gold;
 	unsigned char artifacts;
 	unsigned long hash;
@@ -19,6 +21,8 @@ typedef struct t_game
 
 /* ======================================================================== */
 /* Functions */
+
+void separator( void );
 
 int title( s_game* game );
 int adventure( s_game* game );
@@ -29,44 +33,79 @@ int princess( s_game* game );
 void game_display( s_game* game );
 unsigned long game_get_hash( s_game* game );
 
+int rand_init( void );
+int rand_int( int min, int max);
+
+
 /* ======================================================================== *\
 | Main
 \* ======================================================================== */
 int main( void )
 {
 	s_game game;
-	game_display( &game );
-	title( &game );
-	game_display( &game );
 	
-	bool run = false;
+	fflush( stdout );
+	
+	rand_init();
+	
+	title( &game );
+	
+	
+	bool run = true;
 	while( run )
 	{
-		unsigned char selection = 0;
+		
+		// Selection screen
+		separator();
+		printf( "Day %ld\n", game.days );
+		printf( "Health\t: %d\n", game.health );
+		printf( "Gold\t: %ld\n", game.gold );
+		printf( "Artifacts\t: %d\n", game.artifacts );
+		printf( "\n" );
+		printf( "What to do adventurer?\n" );
+		printf( "[A] go on an Adventure\n" );
+		printf( "[S] Sell my artifacts\n" );
+		printf( "[R] Rest for the night\n" );
+		printf( "[P] marry the Princess!\n" );
+		printf( "[Q] Quit my quest\n" );
+		
+		char selection = 0;
+		//rewind(stdin);
+		fflush( stdin );
+		scanf( "%c", &selection );
+		rewind( stdin );
 		
 		switch( selection )
 		{
-			case 0:
+			case 'A':
 			{
 				adventure( &game );
 				break;
 			}
 			
-			case 1:
+			case 'S':
 			{
 				shop( &game );
 				break;
 			}
 			
-			case 2:
+			case 'R':
 			{
 				rest( &game );
 				break;
 			}
 			
-			case 3:
+			case 'P':
 			{
 				princess( &game );
+				break;
+			}
+			
+			case 'Q':
+			{
+				run = false;
+				separator();
+				printf( "Farewell, adventurer!\n" );
 				break;
 			}
 			
@@ -76,6 +115,8 @@ int main( void )
 				break;
 			}
 		}
+		
+		game_display( &game );
 	}
 	
 	return 0;
@@ -85,9 +126,14 @@ int main( void )
 /* ======================================================================== */
 /* Functions */
 
+void separator( void )
+{
+	printf( "\n  -========================================-\n\n" );
+}
+
 int title( s_game* game )
 {
-	printf( "Title\n" );
+	printf( "XIBRIFI\nCastle\n" );
 	
 	game->days = 0;
 	game->health = 100;
@@ -96,6 +142,10 @@ int title( s_game* game )
 	game->hash = game_get_hash( game );
 	game->initilized = true;
 	
+	printf( "> NEW GAME\n" );
+	printf( "  LOAD\n" );
+	printf( "  QUIT\n" );
+	
 	return 0;
 }
 
@@ -103,6 +153,21 @@ int title( s_game* game )
 int adventure( s_game* game )
 {
 	printf( "Adventure\n" );
+	
+	game->days += 1;
+	
+	game->health -= rand_int( 1, 99 );
+	
+	unsigned char loot = rand_int( 1, 12 );
+	if( game->artifacts > (255-loot) )
+	{
+		game->artifacts = 255;
+	}else{
+		game->artifacts += loot;
+	}
+	
+	game->hash = game_get_hash( game );
+	
 	return 0;
 }
 
@@ -139,7 +204,21 @@ void game_display( s_game* game )
 	printf( "Hash\t: %ld\n", game->hash );
 }
 
+
 unsigned long game_get_hash( s_game* game )
 {
 	return 0;
+}
+
+
+int rand_init( void )
+{
+	srand( time(NULL) );
+	return 0;
+}
+
+
+int rand_int( int min, int max)
+{
+	return min + ( rand()% (max-min) );
 }
